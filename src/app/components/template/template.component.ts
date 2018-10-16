811afa34f28282c3ea40585b1a1358f19c0fbe17
Rafael Manjarres <rafaelmanjarres1995@gmail.com>
2018-10-16 12:13:31 -0500

    nuevo

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -4,16 +4,39 @@ import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+    ` ]
 })
 export class TemplateComponent  {
+  usuario: Object={
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais:"",
+    sexo:null,
+    acepta:false
+  }
+
+  paises=[{
+    codigo:"CRI",
+    nombre:"Costa Rica"},
+  {
+    codigo:"ESP",
+    nombre:"España"
+  }
+]
+  sexos:string[]=["Hombre","Mujer","Sin definir"]
 
   constructor() { }
 
 guardar(forma: NgForm){
-  console.log("Formulario posteado");
+
   console.log("NgForm ",forma);
   console.log("Valor ",forma.value);
+  console.log("usuario", this.usuario);
 }
 
 }
